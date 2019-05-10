44d058083998ea1f7348b10dc8aee33812941d36
David Rodriguez <rojakzym@gmail.com>
2019-05-09 21:43:22 -0500

    se hace a modo de POO el backend, para un poco de orden.

--- backend.php
+++ backend.php
@@ -1,18 +1,23 @@
 <?php
-
-function httpGet($url)
-{
-
+class Backend{
+    public function httpGet($url){
     $curl = curl_init();
-curl_setopt_array($curl, [
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => $url,
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-]);
-$resp = curl_exec($curl);
-curl_close($curl);
-    return $resp;
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $url,
+        CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+    ]);
+    $resp = curl_exec($curl);
+    curl_close($curl);
+        return $resp;
+    }
 }
 header("Accept: application/vnd.github.mercy-preview+json");
-echo httpGet('https://api.github.com/search/repositories?q='.$_REQUEST['q'].'+in:'.$_REQUEST['q'].'');
+$search = new Backend();
+if(isset($_REQUEST["q"])){
+echo  $search->httpGet('https://api.github.com/search/repositories?q='.$_REQUEST['q'].'+in:'.$_REQUEST['q'].'');
+}
+if(isset($_REQUEST["owner"]) && isset($_REQUEST["repoNombre"])){
+echo  $search->httpGet('https://api.github.com/repos/'.$_REQUEST['owner'].'/'.$_REQUEST['repoNombre'].'/pulls/comments');
+}
 ?>
